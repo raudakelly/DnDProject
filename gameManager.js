d012c34ec5ff99a9bfbfc10aa79e0e7728b19ec0
BuildTools <unconfigured@null.spigotmc.org>
2024-01-23 21:30:35 -0600

    updated actionbar

--- gameManager.js
+++ gameManager.js
@@ -6,7 +6,7 @@
     Breakpoint when user selects different action
 
 */
-
+console.log((7 / 10) | 0)
 /*
     Info table
     list of IDs and corresponding data for the skills
@@ -29,18 +29,36 @@ let fishingXp = [10,20,30,40,50,60,70,80];
 */
 
 function beginSkillAction(skillName, action){
+    let abTitle = document.getElementById("ab-title");
+    let abSkillIcon = document.getElementById("ab-skill-icon");
+    let abSkillProgress = document.getElementById("ab-skill-progress");
+    let abProgressBar = document.getElementById("ab-progress-bar");
+    let abProgressBarInner = document.getElementById("ab-progress-bar-inner");
+    let abItemIcon = document.getElementById("ab-item-icon");
+    let abItemQuantity = document.getElementById("ab-item-quantity");
+
     if(skillName == 'woodcutting'){
-        woodcAction(action)
+        abTitle.innerHTML = "Woodcutting"
+        abSkillIcon.setAttribute("src", "./media/sprites/sidebarIcons/tree.png");
+        abSkillIcon.style.height = 64;
+        abSkillIcon.style.width = 64;
+        abItemIcon.setAttribute("src", "./media/sprites/items/woodcutting/3.png");
+        //restart the animation
+        abProgressBarInner.style.animation = "none";
+        abProgressBarInner.offsetHeight;
+        abProgressBarInner.style.animation = null;
     } else if(skillName == 'mining'){
-        console.log("Mining: " + findItemName(action))
+        abTitle.innerHTML = "Mining"
     } else if(skillName == 'smithing'){
+        abTitle.innerHTML = "Smithing"
         //schedule skill event
     } else if(skillName == 'fishing'){
+        abTitle.innerHTML = "Fishing"
         //schedule skill event
     }
     //update actionbar
     let actionBar = document.getElementById("action-bar");
-    actionBar.style.display = "block";
+    actionBar.style.display = "flex";
 }
 
 /*
